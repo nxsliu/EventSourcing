dc359c882e59a063d42bf8d740a92c4367454af4
Nathan Liu <nathan.liu@investec.co.uk>
2015-12-24 11:39:09 +0000

    Created Project

---
+++ Projection/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace Projection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<SubscriberService>(s =>
+                {
+                    s.ConstructUsing(name => new SubscriberService());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+                x.RunAsLocalSystem();
+
+                x.SetDescription("EventSourcing POC");
+                x.SetDisplayName("EventSourcing.Projection");
+                x.SetServiceName("EventSourcing.Projection");
+            });
+        }
+    }
+}
---
+++ Projection/SubscriberService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projection
+{
+    public class SubscriberService
+    {
+        public void Start()
+        {
+
+        }
+
+        public void Stop()
+        {
+        }
+    }
+}
